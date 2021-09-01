91e05c6954ee9b680993a60a53c3c3588d331474
Karan Kanchan <karan.kanchan@morningstar.com>
2021-09-01 22:40:21 +0530

    Insert into table employee_payroll

--- PayrollUC.sql
+++ PayrollUC.sql
@@ -10,3 +10,22 @@ name varchar(255),
 salary int,
 startdate datetime);
 select * from employee_payroll;
+
+/*UC-3_Insert into employee_payroll table*/
+insert into employee_payroll values(
+'Karan',50000,'02/08/2021');
+
+insert into employee_payroll values(
+'Sagar',45000,'03/09/2020');
+
+insert into employee_payroll values(
+'Bhavesh',47000,'12/12/2002');
+
+insert into employee_payroll values(
+'Ruchi',55000,'08/22/2017');
+
+insert into employee_payroll values(
+'Prajakta',39000,'07/30/2020');
+
+insert into employee_payroll values(
+'Rutuja',30000,'12/25/2021');
