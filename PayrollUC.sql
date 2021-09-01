21a242aaccd172d85718dee78996199074f821c4
Karan Kanchan <karan.kanchan@morningstar.com>
2021-09-01 22:36:14 +0530

    Created database

--- PayrollUC.sql
+++ PayrollUC.sql
@@ -1,12 +1,5 @@
-/*UC-1_Create database*/
+/*UC-1_Create database!!*/
 create database payroll_service;
 SELECT name FROM master.sys.databases
 use payroll_service;
 
-/*UC-2_Create Table employee_payroll*/
-create table employee_payroll(
-id int identity(1,1) primary key,
-name varchar(255),
-salary int,
-startdate datetime);
-select * from employee_payroll;
